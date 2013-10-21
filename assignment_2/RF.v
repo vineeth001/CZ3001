08fd35bb7508016cfb82b911d9f8d5cefe8d20f5
liweintu <yang0345@e.ntu.edu.sg>
2013-10-21 10:13:34 +0800

    update RF.v

--- assignment_2/RF.v
+++ assignment_2/RF.v
@@ -19,30 +19,35 @@ module Reg_File (
 	reg [`DSIZE-1:0] RegFile[0:15];
 
 	always@(posedge Clock)
-		if(!Reset)
-			begin
-				RegFile[0]  <= 0;
-				RegFile[1]  <= 0;
-				RegFile[2]  <= 0;
-				RegFile[3]  <= 0;
-				RegFile[4]  <= 0;
-				RegFile[5]  <= 0;
-				RegFile[6]  <= 0;
-				RegFile[7]  <= 0;
-				RegFile[8]  <= 0;
-				RegFile[9]  <= 0;
-				RegFile[10]  <= 0;
-				RegFile[11]  <= 0;
-				RegFile[12]  <= 0;
-				RegFile[13]  <= 0;
-				RegFile[14]  <= 0;
-				RegFile[15]  <= 0;
-			end
-		else
-			RegFile[WAddr] <= ((Wen == 1) && (WAddr != 0)) ? WData : RegFile[WAddr];
-
-	assign RData1 = RegFile[RAddr1];
-	assign RData2 = RegFile[RAddr2];
+		begin
+			if(!Reset)
+				begin
+					RegFile[0]  <= 0;
+					RegFile[1]  <= 0;
+					RegFile[2]  <= 0;
+					RegFile[3]  <= 0;
+					RegFile[4]  <= 0;
+					RegFile[5]  <= 0;
+					RegFile[6]  <= 0;
+					RegFile[7]  <= 0;
+					RegFile[8]  <= 0;
+					RegFile[9]  <= 0;
+					RegFile[10]  <= 0;
+					RegFile[11]  <= 0;
+					RegFile[12]  <= 0;
+					RegFile[13]  <= 0;
+					RegFile[14]  <= 0;
+					RegFile[15]  <= 0;
+				end
+			else
+				RegFile[WAddr] <= ((Wen == 1) && (WAddr != 0)) ? WData : RegFile[WAddr];
+		end
+	
+	// bypass writing
+	// assign RData1 = RegFile[RAddr1];
+	// assign RData2 = RegFile[RAddr2];
+	assign RData1 = ((WAddr == RAddr1) && (WAddr != 0)) ? WData : RegFile[RAddr1];
+	assign RData2 = ((WAddr == RAddr2) && (WAddr != 0)) ? WData : RegFile[RAddr2];
 
 endmodule // End of Reg_File module
 
